32165199e3b5c523bcd6753d7986e47be4313f01
Si-Yoon Bong <bong@granco.co.kr>
2020-10-14 18:13:06 +0900

    dfd

--- CosmeticShampoo.Viewer2/Pages/Dashboard/UserControl_Dashboard_OrderList.xaml.cs
+++ CosmeticShampoo.Viewer2/Pages/Dashboard/UserControl_Dashboard_OrderList.xaml.cs
@@ -1,7 +1,13 @@
-﻿using System;
+﻿using CosmeticShampoo.Viewer2.ViewModels;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,19 +26,128 @@ namespace CosmeticShampoo.Viewer2.Pages
     /// </summary>
     public partial class UserControl_Dashboard_OrderList : UserControl
     {
+        TcpClient clientSocket = new TcpClient(); // 소켓
+
+        NetworkStream stream = default(NetworkStream);
+
+        string message = string.Empty;
+
         UserControl_Dashboard_Total total;
         public UserControl_Dashboard_OrderList(UserControl_Dashboard_Total Total)
         {
             total = Total;
             InitializeComponent();
-            ShowTable();
-        }
-
-        private void ShowTable()
-        {
             
         }
 
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
 
+            {
+
+                IPEndPoint serverAddress = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9991);
+
+                clientSocket.Connect(serverAddress); // 접속 IP 및 포트
+
+                stream = clientSocket.GetStream();
+
+            }
+
+            catch (Exception e2)
+
+            {
+
+                MessageBox.Show("서버가 실행중이 아닙니다.", "연결 실패!");
+
+                Application.Current.Shutdown();
+                Environment.Exit(0);
+
+            }
+
+
+
+
+            byte[] buffer = Encoding.Unicode.GetBytes("테스트 사용자$");
+
+            stream.Write(buffer, 0, buffer.Length);
+
+            stream.Flush();
+
+
+            Thread Get_handler = new Thread(GetData);
+
+            Get_handler.IsBackground = true;
+
+            Get_handler.Start();
+
+
+            Thread Receive_handler = new Thread(SendMessage);
+
+            Receive_handler.IsBackground = true;
+
+            Receive_handler.Start();
+
+
+        }
+
+        private void SendMessage()
+        {
+            while (true)
+            {
+                Thread.Sleep(2000);
+                byte[] buffer = Encoding.Unicode.GetBytes("ShowOrders" + "$");
+                stream.Write(buffer, 0, buffer.Length);
+                stream.Flush();
+            }
+            
+        }
+
+        private void GetData() // 메세지 받기
+        {
+
+            while (true)
+            {
+
+                stream = clientSocket.GetStream();
+                int BUFFERSIZE = clientSocket.ReceiveBufferSize;
+                byte[] buffer = new byte[BUFFERSIZE];
+                int bytes = stream.Read(buffer, 0, buffer.Length);
+
+
+                string message = Encoding.Unicode.GetString(buffer, 0, bytes);
+
+                //Json 거르기
+                if (message.StartsWith("["))
+                {
+
+                    List<Orders> orderlists = new List<Orders>();
+
+                    JArray array = JsonConvert.DeserializeObject<JArray>(message);
+
+                    foreach (JObject data in array)
+                    {
+                        string datas = JsonConvert.SerializeObject(data);
+                        orderlists.Add(JsonConvert.DeserializeObject<Orders>(datas));
+
+                    }
+
+
+                    OrderGrid.Dispatcher.BeginInvoke(new Action(delegate
+                    {
+                        OrderGrid.ItemsSource = null;
+                        OrderGrid.Items.Refresh();
+                        OrderGrid.ItemsSource = orderlists;
+
+                    }));
+
+
+                    continue;
+                }
+
+            }
+
+
+        }
     }
 }
---
+++ CosmeticShampoo.Viewer2/ViewModels/Orders.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosmeticShampoo.Viewer2.ViewModels
+{
+    public class Orders
+    {
+        public int OrderNum { get; set; }
+
+        public string OrderDetail { get; set; }
+
+        public string OrderState { get; set; }
+
+        public string OrderDate { get; set; }
+
+    }
+}
